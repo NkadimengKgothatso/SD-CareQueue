0dbedc29cd4d1c6ebb88993a236759ed319a2e23
NkadimengKgothatso <2840850@Students.wits.ac.za>
2026-04-05 22:02:06 +0200

    Refactor: extract Firebase auth logic into firebase-auth.js
    - Moved Firebase config and auth initialization to separate file  ( firebase.js)
    - removed the sighout button in the dashboard and placed   the side bar

---
+++ Patients WebPages/firebase.js
@@ -0,0 +1,27 @@
+// signup firebse code 
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+    import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+    const firebaseConfig = {
+      apiKey:            "AIzaSyA8a7NhWrtgST9ZY68Dnvxhe8YDyfKqVOA",
+      authDomain:        "carequeue-284bb.firebaseapp.com",
+      projectId:         "carequeue-284bb",
+      storageBucket:     "carequeue-284bb.firebasestorage.app",
+      messagingSenderId: "702048481855",
+      appId:             "1:702048481855:web:1bb9675ecadb9e22043e8a"
+    };
+
+    const app  = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    // If not signed in, send back to login
+    onAuthStateChanged(auth, (user) => {
+      if (!user) window.location.href = "../Login/index.html";
+    });
+
+    // Sign out button
+    window.signOut = async function() {
+      await signOut(auth);
+      window.location.href = "../Login/index.html";
+    };
