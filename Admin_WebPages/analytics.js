284cbd3a2f8cf972b0c20dbca8233a7ab5eb1d02
Sebetola Junior <juniorsebetola253@gmail.com>
2026-04-30 17:00:02 +0200

    Add: Load Appointments from Firestore

--- Admin_WebPages/analytics.js
+++ Admin_WebPages/analytics.js
@@ -1,8 +1,32 @@
 import { initAdminPage, db } from "/Admin_WebPages/admin.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+let appointments = [];
+
+
+//loads all appointments from Firestore because everything depends on them
+async function loadAppointments() {
+    try {
+        const snapshot = await getDocs(collection(db, "Appointments"));
+
+        appointments = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+
+        console.log("Appointments loaded:", appointments);
+
+    } catch (error) {
+        console.error("Error loading appointments:", error);
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  initAdminPage();
+
+    initAdminPage();
+
+    // STEP 1: LOAD DATA
+    loadAppointments();
 
     const form = document.getElementById("filterForm");
 
