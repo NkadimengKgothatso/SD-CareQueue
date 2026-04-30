aeb2925d82eaf15845dd9bc50c2c5c7448306032
Sebetola Junior <juniorsebetola253@gmail.com>
2026-04-30 16:44:48 +0200

    Add:Admin details at the bottom of the sidebar

--- Admin_WebPages/analytics.js
+++ Admin_WebPages/analytics.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initAdminPage, db } from "/Admin_WebPages/admin.js";
 
 const firebaseConfig = {
     apiKey:            "AIzaSyA8a7NhWrtgST9ZY68Dnvxhe8YDyfKqVOA",
