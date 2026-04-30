7e7db1702d93a852816a1b661ea1f4668d9fa07d
Sebetola Junior <juniorsebetola253@gmail.com>
2026-04-30 16:52:56 +0200

    Fix: Sidebar population

--- Admin_WebPages/analytics.js
+++ Admin_WebPages/analytics.js
@@ -1,24 +1,8 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { initAdminPage, db } from "/Admin_WebPages/admin.js";
-
-const firebaseConfig = {
-    apiKey:            "AIzaSyA8a7NhWrtgST9ZY68Dnvxhe8YDyfKqVOA",
-    authDomain:        "carequeue-284bb.firebaseapp.com",
-    projectId:         "carequeue-284bb",
-    storageBucket:     "carequeue-284bb.firebasestorage.app",
-    messagingSenderId: "702048481855",
-    appId:             "1:702048481855:web:1bb9675ecadb9e22043e8a"
-};
-
-const app  = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db   = getFirestore(app);
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  initAdminPage();
 
     const form = document.getElementById("filterForm");
 
