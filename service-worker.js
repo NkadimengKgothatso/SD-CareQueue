04f4f6a5190f1171077cf7de29579cee52883922
LegadimaW <legadimaw@gmail.com>
2026-05-17 10:06:09 +0200

    Added header links to every html

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "carequeue-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/login.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
